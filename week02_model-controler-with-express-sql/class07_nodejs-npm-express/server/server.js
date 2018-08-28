374b4f0641a57343f40549d7474cf0d8b0d103e8
martypdx <marty@martypdx.com>
2018-08-28 10:57:58 -0700

    add basic express route

--- week02_model-controler-with-express-sql/class07_nodejs-npm-express/server/server.js
+++ week02_model-controler-with-express-sql/class07_nodejs-npm-express/server/server.js
@@ -4,16 +4,22 @@ const express = require('express');
 // make an express app. no "new" keyword ¯\_(ツ)_/¯
 const app = express();
 
+// temp solution to updating data...
+const fs = require('fs');
+// path to data file:
+const dataPath = 'data/neighborhoods.json';
 
-app.get('/neighborhoods', (req, res) => {
+// setup a "route":
+// 1) HTTP METHOD, i.e. app.get === for GET requests
+// 2) PATH, i.e. '/api/neighborhoods` === the requested path
+app.get('/api/neighborhoods', (req, res) => {
+  console.log(req.method, req.path);
   res.send([
     { name: 'name1' },
     { name: 'name2' },
     { name: 'name3' }
   ]);
 });
-
-
 
 // set the PORT on which to listen
 const PORT = 3000;
